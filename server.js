584eea3cda89ef750edbbd12d17597b9e3790d6c
imwood04 <41460038+imwood04@users.noreply.github.com>
2020-09-25 09:19:30 -0400

    Redid depicted code

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-var port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname));
 
